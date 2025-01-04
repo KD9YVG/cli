2ee04e7c2904c71cad3ede3235cc93e3a10839be
KD9YVG <Brayden.Irwin.11@gmail.com>
2025-01-04 10:03:35 -0600

    Added basic input handler class

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello World')
+class UserInput:
+    def __init__(self):
+        self.instring = input()
+    
+    def print_input(self):
+        print(self.instring)
+
+for i in range(10):
+    user_input = UserInput()
+    user_input.print_input()

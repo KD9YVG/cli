f7ea138c9aee99c3e43f870125278f5e37b50d09
KD9YVG <Brayden.Irwin.11@gmail.com>
2025-01-11 13:08:35 -0600

    Improve file handling in text editor setup; add error handling for file access and ensure proper resource management.

--- text2.py
+++ text2.py
@@ -1,10 +1,19 @@
-import os
-
 def setup():
     print('\033[H\033[J')
     print('Text editor 0.0.1 Alpha')
     print('-----------------------')
     dir = input('File name/path: ')
-    
+    try:
+        f = open(dir, 'r+')
+    except FileNotFoundError:
+        f = open(dir, 'w+')
+    except PermissionError:
+        print('Permission denied.')
+        return None
+    return f
 
-setup()
+f = setup()
+if f:
+    print(f.read())
+
+f.close()

459a77c9c94dd260439d1cfff3914b857fd3e431
KD9YVG <Brayden.Irwin.11@gmail.com>
2025-02-02 21:12:06 -0600

    Add cmdIdentify and cmdhandle classes; implement command identification from file input

--- Files/cmdhandle.py
+++ Files/cmdhandle.py
@@ -1,4 +1,7 @@
-class Identify:
+global run
+run = False
+
+class cmdIdentify:
     def __init__(self, string=''):
         self.instring = ""
         self.command = ""
@@ -7,6 +10,8 @@ class Identify:
             self.instring = string
             self.words = string.split()
             self.command = self.words[0]
+            global run
+            run = True
             
     def demo(self, instring):
         self.instring = instring
@@ -22,3 +27,21 @@ class Identify:
 
     def get_command(self):
         return self.command
+
+class cmdhandle:
+    def identify(self, instring):
+        global run
+        if run == True:
+            command = cmdIdentify.get_command()
+            with open('Files/commands.txt', 'r') as file:
+                # read the file line by line
+                for line_num, line in enumerate(file, 1):
+                    # check if the command is in the file
+                    if command in line:
+                        print(f"Line {line_num}: {line}")
+                        break
+                else:
+                    print('Command not found')
+
+cmdIdentify(input())
+cmdhandle().identify(input())

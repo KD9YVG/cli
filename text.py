03cc37b1e37df44f5c70c7703623ac9265c0f49b
KD9YVG <Brayden.Irwin.11@gmail.com>
2025-03-29 09:12:59 -0500

    text.py now takes up full screen

--- text.py
+++ text.py
@@ -1,10 +1,19 @@
 import blessed
+import time
 
 term = blessed.Terminal()
 
 class Text:
     def __init__(self):
-        print(term.center(term.on_white(term.black('pyText 0.0.1 Alpha'))))
+        print(term.enter_fullscreen())
+        print(term.clear)
+        print(term.on_white(term.black('pyText 0.0.1 Alpha')))
         print(term.move_down)
+        print(term.move_xy(0, term.height - 1) + term.on_white(term.black('Ctrl+X - Close    Ctrl+S - Save')))
 
-Text()
+if __name__ == "__main__":
+    try:
+        Text()
+    finally:
+        time.sleep(2)
+        print(term.exit_fullscreen())
